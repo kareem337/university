c2335e3a7987f475468be3a7248672f179177574
Kareem Yasser Samir Razzaz <74376900+kareem337@users.noreply.github.com>
2024-08-28 18:14:44 +0300

    remove commented code

--- university/src/main/java/com/universityapp/university/UniversityApplication.java
+++ university/src/main/java/com/universityapp/university/UniversityApplication.java
@@ -10,23 +10,6 @@ public class UniversityApplication {
 	// swagger link: http://localhost:8080/swagger-ui/index.html
 	public static void main(String[] args) {
 		SpringApplication.run(UniversityApplication.class, args);
-		//Testing Quiz app OOP
-//		Quiz quiz = new Quiz();
-//
-//		// Add some sample questions
-//		quiz.addQuestion(new Question("Where is Sumerge located?",
-//				new String[]{"Maadi", "New Cairo", "Mokattam", "Nasr City"}, "Maadi"));
-//		quiz.addQuestion(new Question("What is the capital of Egypt?",
-//				new String[]{"Cairo", "Giza", "Alexandria", "Aswan"}, "Cairo"));
-//		quiz.addQuestion(new Question("What is 2 + 2?",
-//				new String[]{"3", "4", "5", "6"}, "4"));
-//
-//		Scanner scanner = new Scanner(System.in);
-//		System.out.print("Enter your name: ");
-//		String name = scanner.nextLine();
-//
-//		User user = new User(name, quiz);
-//		user.takeQuiz();
 	}
 
 }
